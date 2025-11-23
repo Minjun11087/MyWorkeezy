9c11599185461abb73686d87afc49fa68397f303
HeizlHazel <hyejee0221@gmail.com>
2025-11-23 22:33:13 +0900

    프로젝트 생성

---
+++ workeezy/src/main/java/com/together/workeezy/WorkeezyApplication.java
@@ -0,0 +1,11 @@
+package com.together.workeezy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkeezyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkeezyApplication.class, args); }
+}
---
+++ workeezy/src/test/java/com/together/workeezy/WorkeezyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.together.workeezy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkeezyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
