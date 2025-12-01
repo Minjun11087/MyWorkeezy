559ca99dcdef80f243199e9a374d77e558afda09
HeizlHazel <hyejee0221@gmail.com>
2025-12-01 19:16:08 +0900

    :memo: BCrypt 암호화 테스트 코드

--- workeezy/src/main/java/com/together/workeezy/WorkeezyApplication.java
+++ workeezy/src/main/java/com/together/workeezy/WorkeezyApplication.java
@@ -1,11 +1,29 @@
 package com.together.workeezy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class WorkeezyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkeezyApplication.class, args); }
+//    private final PasswordEncoder passwordEncoder;
+//
+//    public WorkeezyApplication(PasswordEncoder passwordEncoder) {
+//        this.passwordEncoder = passwordEncoder;
+//    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(WorkeezyApplication.class, args);
+    }
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner() {
+//        return args -> {
+//            System.out.println(passwordEncoder.encode("950218"));
+//        };
+//    }
+
 }
