7d37d8263810c8d1f8fdd914398f2430200fbfab
HeizlHazel <hyejee0221@gmail.com>
2025-12-02 18:46:03 +0900

    📝 import 수정

--- workeezy-react/src/pages/ProgramDetailPage.jsx
+++ workeezy-react/src/pages/ProgramDetailPage.jsx
@@ -1,4 +1,3 @@
-import PageLayout from "../layout/PageLayout";
 import FloatingButtons from "../components/Common/FloatingButtons.jsx";
 import ProgramTitle from "../components/Program/ProgramTitle.jsx";
 import ProgramImages from "../components/Program/ProgramImages.jsx";
@@ -7,9 +6,8 @@ import ProgramTabs from "../components/Program/ProgramTabs.jsx";
 import ProgramInfo from "../components/Program/ProgramInfo.jsx";
 import HotelInfo from "../components/ProgramDetails/HotelInfo.jsx";
 import RoomList from "../components/ProgramDetails/RoomList.jsx";
-import OfficeList from "../components/OfficeList/OfficeList.jsx";
-import ActivityInfo from "../components/ActivityInfo/ActivityInfo.jsx";
-
+import OfficeList from "../components/ProgramDetails/OfficeList.jsx";
+import ActivityInfo from "../components/ProgramDetails/ActivityInfo.jsx";
 
 export default function ProgramDetailPage() {
   const mainImage = "/public/ac95ce1d-57d6-4862-9e4e-fabfadd1e5a2.png";
@@ -20,18 +18,18 @@ export default function ProgramDetailPage() {
     "public/db333417-b310-4ba1-ac03-33cd2b71f553.png",
   ];
 
-    return (
-        <PageLayout>
-            <ProgramTitle title="부산 영도 워케이션"/>
-            <ProgramImages main={mainImage} subs={subImages}/>
-            <ProgramReserveBar/>
-            <ProgramTabs/>
-            <ProgramInfo/>
-            <HotelInfo/>
-            <RoomList/>
-            <OfficeList/>
-            <ActivityInfo/>
-            <FloatingButtons/>
-        </PageLayout>
-    );
+  return (
+    <PageLayout>
+      <ProgramTitle title="부산 영도 워케이션" />
+      <ProgramImages main={mainImage} subs={subImages} />
+      <ProgramReserveBar />
+      <ProgramTabs />
+      <ProgramInfo />
+      <HotelInfo />
+      <RoomList />
+      <OfficeList />
+      <ActivityInfo />
+      <FloatingButtons />
+    </PageLayout>
+  );
 }
