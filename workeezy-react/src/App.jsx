f3ab665508aa098f49ce7003e1234460f8553501
HeizlHazel <hyejee0221@gmail.com>
2025-12-02 15:41:01 +0900

    📝 탭 간격 수정

--- workeezy-react/src/App.jsx
+++ workeezy-react/src/App.jsx
@@ -6,19 +6,14 @@ import SearchResultPage from "./pages/SearchPage";
 import SearchPage from "./pages/SearchPage";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-
-                {/* 기본 URL → 로그인 페이지로 리다이렉트 */}
-                <Route path="/" element={<Navigate to="/login" replace />} />
-
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/program" element={<ProgramDetailPage />} />
-                <Route path="/reviews" element={<ReviewPage />} />
-                <Route path="/search" element={<SearchPage/>} />
-
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <Routes>
+      {/* 기본 URL → 로그인 페이지로 리다이렉트 */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/program" element={<ProgramDetailPage />} />
+      <Route path="/reviews" element={<ReviewPage />} />
+      <Route path="/search" element={<SearchPage />} />
+    </Routes>
+  );
 }
