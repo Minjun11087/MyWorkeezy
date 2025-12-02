cde807f534cd834a2cf31091d09b827c953ef9d3
HeizlHazel <hyejee0221@gmail.com>
2025-12-02 14:53:14 +0900

    :memo: Home 코드 수정

---
+++ workeezy-react/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import PageLayout from "../layout/PageLayout.jsx";
+import SearchBar from "../components/Common/SearchBar.jsx";
+
+export default function Home() {
+  return (
+    <PageLayout>
+      <SearchBar />
+    </PageLayout>
+  );
+}
