9a6b69f75775a10aff4364b6d9db9560e396d132
HeizlHazel <hyejee0221@gmail.com>
2025-12-03 17:47:51 +0900

    :memo: 리액트 허용 url 추가

--- workeezy/src/main/java/com/together/workeezy/config/SecurityConfig.java
+++ workeezy/src/main/java/com/together/workeezy/config/SecurityConfig.java
@@ -65,6 +65,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://localhost:5174");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
