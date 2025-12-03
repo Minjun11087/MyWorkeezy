498cefefd265f7b10ad476ad206a3538bd90f40d
HeizlHazel <hyejee0221@gmail.com>
2025-12-03 15:00:40 +0900

    :wrench: axios api 코드 수정

--- workeezy-react/src/api/axios.js
+++ workeezy-react/src/api/axios.js
@@ -2,15 +2,19 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080",
+  withCredentials: true, // refreshToken 쿠키 사용하려면 필요
 });
 
 // 요청 인터셉터 설정 → 모든 요청에 자동으로 JWT 추가
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
