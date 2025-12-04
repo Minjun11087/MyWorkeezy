d8a17dcc27f98648cf30af2919db3bb4cfee5eb8
HeizlHazel <hyejee0221@gmail.com>
2025-12-04 18:48:34 +0900

    📝 import 변경

--- workeezy-react/src/pages/MyPage.jsx
+++ workeezy-react/src/pages/MyPage.jsx
@@ -1,5 +1,5 @@
 import PageLayout from "../layout/PageLayout";
-import ProfileForm from "../components/Common/ProfileForm.jsx";
+import ProfileForm from "../components/Profile/ProfileForm.jsx";
 
 export default function MyPage() {
   return (
