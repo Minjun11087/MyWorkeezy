9938fd2363d7ebafb1b9bbaffa211a47469ffb8a
HeizlHazel <hyejee0221@gmail.com>
2025-12-04 17:17:49 +0900

    :wrench: 인증 관련 페이지 route 수정

--- workeezy-react/src/App.jsx
+++ workeezy-react/src/App.jsx
@@ -1,38 +1,54 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import PrivateRoute from "./route/PrivateRoute";
+import ProfileGuard from "./route/ProfileGuard";
+
 import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
+import ProfilePasswordCheck from "./pages/ProfilePasswordCheck";
+
 import ProgramDetailPage from "./pages/ProgramDetailPage";
 import ReviewPage from "./pages/ReviewPage";
 import SearchPage from "./pages/SearchPage";
-import NewReservationPage from "./pages/NewReservationPage.jsx";
-import ModifyReservationPage from "./pages/ModifyReservationPage.jsx";
-import MyPage from "./pages/MyPage.jsx";
-import Home from "./pages/Home.jsx";
-import ProfilePasswordCheck from "./pages/ProfilePasswordCheck.jsx";
-import ProfileGuard from "./components/Auth/ProfileGuard.jsx";
+import NewReservationPage from "./pages/NewReservationPage";
+import ModifyReservationPage from "./pages/ModifyReservationPage";
+
 
 export default function App() {
-  return (
-    <Routes>
-      {/* 기본 URL → 로그인 페이지로 리다이렉트 */}
-      {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/profile"
-        element={
-          <ProfileGuard>
-            <MyPage />
-          </ProfileGuard>
-        }
-      />
-      <Route path="/profilecheck" element={<ProfilePasswordCheck />} />
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
 
-      <Route path="/program" element={<ProgramDetailPage />} />
-      <Route path="/reviews" element={<ReviewPage />} />
-      <Route path="/search" element={<SearchPage />} />
+            {/* 비밀번호 재확인 단계 */}
+            <Route
+                path="/profile-check"
+                element={
+                    <PrivateRoute>
+                        <ProfilePasswordCheck />
+                    </PrivateRoute>
+                }
+            />
 
-      <Route path="/newreservation" element={<NewReservationPage />} />
-      <Route path="/modifyreservation" element={<ModifyReservationPage />} />
-    </Routes>
-  );
+            {/* 마이페이지 */}
+            <Route
+                path="/profile"
+                element={
+                    <PrivateRoute>
+                        <ProfileGuard>
+                            <MyPage />
+                        </ProfileGuard>
+                    </PrivateRoute>
+                }
+            />
+
+            {/* 기타 페이지 */}
+            <Route path="/program" element={<ProgramDetailPage/>}/>
+            <Route path="/reviews" element={<ReviewPage/>}/>
+            <Route path="/search" element={<SearchPage/>}/>
+            <Route path="/newreservation" element={<NewReservationPage/>}/>
+            <Route path="/modifyreservation" element={<ModifyReservationPage/>}/>
+        </Routes>
+    );
 }
