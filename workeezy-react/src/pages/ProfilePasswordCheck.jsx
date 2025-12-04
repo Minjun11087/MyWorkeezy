f65a6ed838a113929999e96ad8312129a0db47f0
HeizlHazel <hyejee0221@gmail.com>
2025-12-04 18:49:38 +0900

    :wrench: 비밀번호 검증 - 페이지 컴포넌트 분리

--- workeezy-react/src/pages/ProfilePasswordCheck.jsx
+++ workeezy-react/src/pages/ProfilePasswordCheck.jsx
@@ -1,55 +1,10 @@
 import PageLayout from "../layout/PageLayout";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import api from "../api/axios";
+import ProfilePasswordCheckForm from "../components/profile/ProfilePasswordCheckForm";
 
 export default function ProfilePasswordCheck() {
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-    console.log("로컬스토리지 토큰:", localStorage.getItem("accessToken"));
-
-
-    const handleCheck = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await api.post(
-        "/api/auth/check-password",
-        {
-          password,
-        }
-      );
-
-      if (res.data.success) {
-        // 🔥 인증 성공 저장
-        localStorage.setItem("profileVerified", "true");
-
-        // 🔥 마이페이지로 이동
-        navigate("/profile", { replace: true });
-      } else {
-        alert("비밀번호가 일치하지 않습니다.");
-      }
-    } catch (err) {
-      console.error(err);
-      alert("오류 발생");
-    }
-  };
-
-  return (
-    <div className="profile-check-wrapper">
-      <form onSubmit={handleCheck} className="profile-check-form">
-        <h2>비밀번호 확인</h2>
-
-        <input
-          type="password"
-          placeholder="비밀번호를 입력하세요"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button type="submit">확인</button>
-      </form>
-    </div>
-  );
+    return (
+        <PageLayout>
+            <ProfilePasswordCheckForm />
+        </PageLayout>
+    );
 }
