ba6d8810eecf90899adc2096df2b469d32ea07a2
Hyejee Kim <hyejee0221@gmail.com>
2025-12-08 18:36:18 +0900

    🔧 자동 로그인 코드 수정

--- workeezy-react/src/App.jsx
+++ workeezy-react/src/App.jsx
@@ -19,12 +19,37 @@ import AdimnReservationListPage from "./features/reservation/pages/AdminReservat
 import Forbidden from "./shared/error/Forbidden.jsx";
 import ServerError from "./shared/error/ServerError.jsx";
 import NotFound from "./shared/error/NotFound.jsx";
+import {useEffect} from "react";
+import api from "./api/axios.js";
 
 export default function App() {
   if (window.Kakao && !window.Kakao.isInitialized()) {
     window.Kakao.init("b915b18542b9776646e5434c83e959c9");
     console.log("Kakao SDK initialized!");
   }
+
+  useEffect(() => {
+      const auto = localStorage.getItem("autoLogin  ");
+
+      // 자동 로그인 미체크 시 패스
+      if (auto !== "true") return;
+
+      // accessToken이 이미 있으면 패스
+      if (localStorage.getItem("accessToken")) return;
+
+      // 자동 로그인 체크시에만 refresh 시도
+      api.post("/api/auth/refresh")
+          .then(res => {
+              const newToken = res.data.token;
+              localStorage.setItem("accessToken", newToken);
+              console.log("자동 로그인 성공(refresh 재발급 완료)");
+          })
+          .catch(err => {
+              console.log("자동 로그인 실패: ", err);
+              localStorage.removeItem("autoLogin");
+              // 실패 시 로그인 페이지로 리다이렉트 또는 무시
+          });
+  }, []);
 
   return (
     <Routes>
