d178a656892b2e56151d7945a73ed941ba4a6630
Hyejee Kim <hyejee0221@gmail.com>
2025-12-10 11:15:29 +0900

    🔥 review_title 필드 삭제

--- workeezy/src/main/java/com/together/workeezy/program/entity/Review.java
+++ workeezy/src/main/java/com/together/workeezy/program/entity/Review.java
@@ -3,7 +3,6 @@ package com.together.workeezy.program.entity;
 import com.together.workeezy.user.entity.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
@@ -30,11 +29,6 @@ public class Review {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    @Size(max = 100)
-    @NotNull
-    @Column(name = "review_title", nullable = false, length = 100)
-    private String title;
 
     @Lob
     @Column(name = "review_content")
