c717041eea1dc24476dec3ed117b369feb67338a
HeizlHazel <hyejee0221@gmail.com>
2025-12-31 20:21:26 +0900

    🔧 S3Config 수정

--- workeezy/src/main/java/com/together/workeezy/config/S3Config.java
+++ workeezy/src/main/java/com/together/workeezy/config/S3Config.java
@@ -1,4 +1,5 @@
 package com.together.workeezy.config;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
@@ -6,25 +7,20 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
     public AmazonS3 amazonS3() {
-        BasicAWSCredentials credentials =
-                new BasicAWSCredentials(accessKey, secretKey);
-
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                // credentials 설정 없음
                 .build();
     }
 }
