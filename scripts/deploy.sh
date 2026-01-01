cf59b1b7bac7e2772cedecd364a87f424e5bf36b
HeizlHazel <hyejee0221@gmail.com>
2026-01-02 02:24:53 +0900

    🔧 쉘스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,22 +3,21 @@ set -e
 
 APP_DIR=/home/ubuntu/workeezy-backend
 REGION=ap-northeast-2
-ECR_REGISTRY=002177417362.dkr.ecr.ap-northeast-2.amazonaws.com
-
+ECR_REGISTRY=020513637952.dkr.ecr.ap-northeast-2.amazonaws.com/workeezy-server
 echo "Move to app directory"
 cd $APP_DIR
 
 echo "ECR login"
 aws ecr get-login-password --region $REGION \
- | docker login --username AWS --password-stdin $ECR_REGISTRY
+ | sudo docker login --username AWS --password-stdin $ECR_REGISTRY
 
 echo "Pull latest images"
-docker compose pull
+sudo docker compose pull
 
 echo "Stop & remove old containers"
-docker compose down --remove-orphans
+sudo docker compose down --remove-orphans
 
 echo "Start containers (app + redis)"
-docker compose up -d
+sudo docker compose up -d
 
 echo "Deploy finished"
